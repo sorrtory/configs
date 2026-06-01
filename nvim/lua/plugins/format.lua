d377707ba9048dae139fbe5260391735a2a80be0
sorrtory <62436169+sorrtory@users.noreply.github.com>
2026-06-01 23:40:28 +0300

    nvim: add linter

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,99 +1,100 @@
 -- ~/.config/nvim/lua/plugins/format.lua
 
 return {
-  {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
 
-    keys = {
-      {
-        "<leader>lf",
-        function()
-          require("conform").format({
-            async = true,
-            lsp_format = "fallback",
-          })
-        end,
-        desc = "Format file",
-      },
-    },
+		keys = {
+			{
+				"<leader>lf",
+				function()
+					require("conform").format({
+						async = true,
+						lsp_format = "fallback",
+					})
+				end,
+				desc = "Format file",
+			},
+		},
 
-    opts = {
-      notify_on_error = true,
+		opts = {
+			notify_on_error = true,
 
-      format_on_save = {
-        timeout_ms = 1000,
-        lsp_format = "fallback",
-      },
+			format_on_save = {
+				timeout_ms = 1000,
+				lsp_format = "fallback",
+			},
 
-      formatters_by_ft = {
-        -- Lua
-        lua = { "stylua" },
+			formatters_by_ft = {
+				-- Lua
+				lua = { "stylua" },
 
-        -- Web / JS / TS
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-        typescript = { "prettierd", "prettier", stop_after_first = true },
-        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				-- Web / JS / TS
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 
-        html = { "prettierd", "prettier", stop_after_first = true },
-        css = { "prettierd", "prettier", stop_after_first = true },
-        scss = { "prettierd", "prettier", stop_after_first = true },
-        json = { "prettierd", "prettier", stop_after_first = true },
-        jsonc = { "prettierd", "prettier", stop_after_first = true },
-        yaml = { "prettierd", "prettier", stop_after_first = true },
-        markdown = { "prettierd", "prettier", stop_after_first = true },
-        markdown_inline = { "prettierd", "prettier", stop_after_first = true },
+				html = { "prettierd", "prettier", stop_after_first = true },
+				css = { "prettierd", "prettier", stop_after_first = true },
+				scss = { "prettierd", "prettier", stop_after_first = true },
+				json = { "prettierd", "prettier", stop_after_first = true },
+				jsonc = { "prettierd", "prettier", stop_after_first = true },
+				yaml = { "prettierd", "prettier", stop_after_first = true },
+				markdown = { "prettierd", "prettier", stop_after_first = true },
+				markdown_inline = { "prettierd", "prettier", stop_after_first = true },
 
-        -- Python
-        python = { "ruff_format", "ruff_organize_imports" },
+				-- Python
+				python = { "ruff_format", "ruff_organize_imports" },
 
-        -- Go
-        go = { "goimports", "gofumpt" },
+				-- Go
+				go = { "goimports", "gofumpt" },
 
-        -- Shell
-        sh = { "shfmt" },
-        bash = { "shfmt" },
+				-- Shell
+				sh = { "shfmt" },
+				bash = { "shfmt" },
 
-        -- C / C++
-        c = { "clang_format" },
-        cpp = { "clang_format" },
+				-- C / C++
+				c = { "clang_format" },
+				cpp = { "clang_format" },
 
-        -- Rust: use rustfmt if available, otherwise rust-analyzer LSP fallback
-        rust = { "rustfmt", lsp_format = "fallback" },
-      },
-    },
-  },
+				-- Rust: use rustfmt if available, otherwise rust-analyzer LSP fallback
+				rust = { "rustfmt", lsp_format = "fallback" },
+			},
+		},
+	},
 
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = {
-      "mason-org/mason.nvim",
-    },
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			"mason-org/mason.nvim",
+		},
 
-    opts = {
-      ensure_installed = {
-        -- Lua
-        "stylua",
+		opts = {
+			ensure_installed = {
+				-- Lua
+				"stylua",
 
-        -- Web / JS / TS
-        "prettierd",
-        "prettier",
+				-- Web / JS / TS
+				"prettierd",
+				"prettier",
+				"htmlhint",
 
-        -- Python
-        "ruff",
+				-- Python
+				"ruff",
 
-        -- Go
-        "goimports",
-        "gofumpt",
+				-- Go
+				"goimports",
+				"gofumpt",
 
-        -- Shell
-        "shfmt",
+				-- Shell
+				"shfmt",
 
-        -- C / C++
-        "clang-format",
-      },
-    },
-  },
+				-- C / C++
+				"clang-format",
+			},
+		},
+	},
 }
